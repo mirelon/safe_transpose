88a399fee7c8d9502dcea9ebaee42713adf8e68f
Miso Kovac <kovac@essential-data.sk>
2014-11-15 10:54:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SafeTranspose::Application.config.secret_token = 'ef58285769796333f3910ff756bad9b365bf8d8a4c58e369fe031e1c1edc5a252214b9022affee4bb960caef817c5360728831687182e88fd24a686ee650def7'
